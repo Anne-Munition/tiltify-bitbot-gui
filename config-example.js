4f6d6c3431c7afc827a440c450afa2465d7470a1
DBKynd <db@dbkynd.com>
2022-06-02 21:02:17 -0600

    Initial Commit

---
+++ config-example.js
@@ -0,0 +1,10 @@
+// RENAME THIS FILE TO: config.js
+
+// Tiltify Application Access Token.
+// https://dashboard.tiltify.com/YOUR_USERNAME/my-account/connected-accounts/applications
+const tiltifyToken = "123456789";
+
+// Twitch chat OAuth Token.
+// https://twitchapps.com/tmi/
+// Include the 'oauth:'
+const twitchToken = "oauth:123456789";
